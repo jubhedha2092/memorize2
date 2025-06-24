1514347b4c722bd04991e943d3fd3d33cea6b639
Jubhedha Shaik <jubhedha17@gmail.com>
2025-06-23 23:04:10 -0400

    Initial Commit

---
+++ memorize2/memorize2App.swift
@@ -0,0 +1,17 @@
+//
+//  memorize2App.swift
+//  memorize2
+//
+//  Created by Jubhedha Shaik on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct memorize2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ memorize2Tests/memorize2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  memorize2Tests.swift
+//  memorize2Tests
+//
+//  Created by Jubhedha Shaik on 6/23/25.
+//
+
+import Testing
+@testable import memorize2
+
+struct memorize2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
